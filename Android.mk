d443ae66b27c129f178177be53b56b94d54bc44e
sdhz151 <yanghui140@gmail.com>
2017-02-19 17:12:49 +0800

    Revert "SetupWizard: Part 2: Implement new OOBE using custom wizard manager"
    This reverts commit d4cea55dd7f5eeaa2494fbd369a113c282f06c64.

--- Android.mk
+++ Android.mk
@@ -5,10 +5,9 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PACKAGE_NAME := LineageSetupWizard
+LOCAL_PACKAGE_NAME := CyanogenSetupWizard
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
-LOCAL_OVERRIDES_PACKAGES := Provision
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
